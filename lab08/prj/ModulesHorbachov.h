c811af1a6afe891926dd034a7452941409199b7b
Evgeniy Gorbachov <zekagor@gmail.com>
2023-04-04 22:18:46 +0300

    Was added report to work

--- lab08/prj/ModulesHorbachov.h
+++ lab08/prj/ModulesHorbachov.h
@@ -3,4 +3,10 @@
 
 float s_calculation(float x, float y, float z);
 
+float task9_1(float salary, int years);
+
+std::string task9_2(int lengthHead);
+
+int task9_3(int number);
+
 #endif // MODULESHORBACHOV_H_INCLUDED
