63f1181cca5bce297ef3086c77c0b68d3f1a30ad
Evgeniy Gorbachov <zekagor@gmail.com>
2023-04-05 22:42:38 +0300

    Was added a folder to lab9 prj, with lib, which need only to see thems code.

---
+++ lab09/prj/ModulesHorbachov.h
@@ -0,0 +1,12 @@
+#ifndef MODULESHORBACHOV_H_INCLUDED
+#define MODULESHORBACHOV_H_INCLUDED
+
+float s_calculation(float x, float y, float z);
+
+float task9_1(float salary, int years);
+
+std::string task9_2(int lengthHead);
+
+int task9_3(int number);
+
+#endif // MODULESHORBACHOV_H_INCLUDED
